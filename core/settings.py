d288d484e9a6e01dedfa90c53b8cacb4658bb24f
Calebokyere2009 <calebokyeredanquah0@gmail.com>
2025-11-15 23:58:17 +0000

    I changed just the NAME

--- core/settings.py
+++ core/settings.py
@@ -74,8 +74,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'geo_env',
         'USER':'postgres',
         'PASSWORD':'carlsagan1992',
         'HOST':'localhost',

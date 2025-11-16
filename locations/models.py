fa30d0e24c9a8dd64ffaa051495735d99b9448db
Calebokyere2009 <calebokyeredanquah0@gmail.com>
2025-11-16 00:37:45 +0000

    I kinda created a PointField to give the features indices

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class PointOfInterest(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    location = models.PointField(srid=4326)  # WGS 84 latitude/longitude
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name

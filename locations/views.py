0e3222cdccb4452ed31730c341ef4ad24f53a54f
Calebokyere2009 <calebokyeredanquah0@gmail.com>
2025-12-19 21:54:32 +0000

    API views were imported and utilized in the locations/views.py

--- locations/views.py
+++ locations/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Location
+from .serializers import LocationSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def location_list(request):
+    locations = Location.objects.all()
+    serializer = LocationSerializer(locations, many=True)
+    return Response(serializer.data)

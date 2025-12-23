2bdf8af1ca90de00374a83dd8fd2d009bc90bea4
Calebokyere2009 <calebokyeredanquah0@gmail.com>
2025-12-23 23:39:15 +0000

    Hmm. Still a bit confused but i added a list to the fields and created a create function to create lat,lon variables

--- locations/serializers.py
+++ locations/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import Location
+from django.contrib.gis.geos import Point
 
-class LocationSerializer(serializers.ModelSerializer):  
+class LocationSerializer(serializers.ModelSerializer): 
+    latitude = serializers.FloatField(write_only=True)
+    longitude = serializers.FloatField(write_only=True) 
     class Meta:
         model = Location
-        fields = "__all__"  
+        fields = ['id', 'name', 'location', 'latitude', 'longitude']
+
+        def create(self, validated_data):
+            latitude = validated_data.pop('latitude')
+            longitude = validated_data.pop('longitude')
+            
